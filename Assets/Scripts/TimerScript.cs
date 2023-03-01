2b8572b5af9730a9a4558f77a5d27a8d4ecd7799
valgardg <valgardo20@ru.is>
2023-03-01 21:29:04 +0000

    added potential clock functionality

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -7,7 +7,7 @@ using System;
 
 public class TimerScript : MonoBehaviour
 {
-    public TextMeshProUGUI timerText;
+    private TextMeshProUGUI timerText;
     private float startTime;
 
     void Awake()
@@ -25,11 +25,14 @@ public class TimerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var difference = Time.time - startTime;
-        string minutes = ((int)difference / 60).ToString();
-        string seconds = (difference % 60).ToString("f0");
-        string timeString = minutes + "m " + seconds +"s";
-        //Debug.Log(timeString);
-        timerText.text = timeString;
+        timerText = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
+        if(timerText){
+            var difference = Time.time - startTime;
+            string minutes = ((int)difference / 60).ToString();
+            string seconds = (difference % 60).ToString("f0");
+            string timeString = minutes + "m " + seconds +"s";
+            //Debug.Log(timeString);
+            timerText.text = timeString;
+        }
     }
 }
